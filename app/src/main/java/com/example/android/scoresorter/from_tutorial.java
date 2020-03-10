441395c58f8230b3510387657f53d14fe480a799
dcaldr <22105838+dcaldr@users.noreply.github.com>
2020-03-10 10:56:11 +0100

    Update from_tutorial.java

--- app/src/main/java/com/example/android/scoresorter/from_tutorial.java
+++ app/src/main/java/com/example/android/scoresorter/from_tutorial.java
@@ -6,3 +6,4 @@ for (int i = 0; i < CustomeAdapter.editModelArrayList.size(); i++){
  
         }
 /* TODO: add to @EditModel new method to get double or float[less decimal numbers] (as double not String) to be used in @MagicCalculate */
+/* Test @MagicCalculate*/
