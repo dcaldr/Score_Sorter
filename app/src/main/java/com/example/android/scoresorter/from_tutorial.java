b35d0a9b96e1b4423ccff05468f3956adabd158e
dcaldr <22105838+dcaldr@users.noreply.github.com>
2020-03-10 10:19:13 +0100

    Create from_tutorial.java
    Idea from tutorial, that might help.

---
+++ app/src/main/java/com/example/android/scoresorter/from_tutorial.java
@@ -0,0 +1,7 @@
+String convertText;
+for (int i = 0; i < CustomeAdapter.editModelArrayList.size(); i++){
+ 
+               CustomeAdapter.editModelArrayList.get(i).getEditTextValue() ;
+            //convertText =    CustomeAdapter.editModelArrayList.get(i).getFinalScore();  
+ 
+        }
